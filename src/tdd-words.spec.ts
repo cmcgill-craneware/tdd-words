1e8aaf5d6574e1833a8add88a1ef8ec1b7c2ce72
CW-DATA\Connor McGill <c.mcgill@craneware.com>
2018-04-05 10:18:53 +0100

    Initial commit, clears out uneeded visual studio solution.

---
+++ src/tdd-words.spec.ts
@@ -0,0 +1,10 @@
+import { TddWords } from './tdd-words';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('tdd words ',() => {
+    let words;
+    beforeEach(() => {
+        words = new TddWords;
+    });
+})
---
+++ src/tdd-words.ts
@@ -0,0 +1,3 @@
+export class TddWords {
+
+};
