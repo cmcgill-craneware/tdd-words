7e922f775adc2e5183036836e64579cf5d9a7bcb
CW-DATA\Connor McGill <c.mcgill@craneware.com>
2018-04-06 11:45:36 +0100

    tdd-kata homework

--- src/tdd-words.spec.ts
+++ src/tdd-words.spec.ts
@@ -7,4 +7,47 @@ describe('tdd words ',() => {
     beforeEach(() => {
         words = new TddWords;
     });
+
+    it('should return zero when string is null', () => {
+        expect(words.wordCount(null)).to.equal(0)
+    });
+
+    it('should return zero when string consists of only whitespaces', () => {
+        expect(words.wordCount(' \n\n\t\r ')).to.equal(0)
+    });
+
+    it('should return 1 with a single word', () => {
+        expect(words.wordCount('Hello')).to.equal(1)
+    });
+
+    it('should return 2 with two words separated by a space', () => {
+        expect(words.wordCount('Hello world')).to.equal(2)
+    });
+
+    it('should return 1 with single word separated by whitespaces', () => {
+        expect(words.wordCount('  \nHello  \r ')).to.equal(1)
+    });
+
+    it('should return correct word count when words separated by any whitespaces', () => {
+        expect(words.wordCount('Hello crazy \n  world')).to.equal(3)
+    });
+
+    it('should count only unique words and return 3', () => {
+        expect(words.wordCount('Hello crazy \n  world')).to.equal(3)
+    });
+
+    it('should count only unique case insentive words and return 2', () => {
+        expect(words.wordCount('Hello world hello')).to.equal(2)
+    });
+
+    it('should ignore trailing periods and return 2', ()=> {
+        expect(words.wordCount('Hello world hello.')).to.equal(2)
+    });
+
+    it('should ignore all punction and return 3', () => {
+        expect(words.wordCount('Hello (world) - 123.')).to.equal(3)
+    });
+    it('should count words that are accent insensitive and return 4', () => {
+        expect(words.wordCount('Allô? Allo est la fin.')).to.equal(4)
+    });
 })
--- src/tdd-words.ts
+++ src/tdd-words.ts
@@ -1,3 +1,47 @@
 export class TddWords {
+    wordCount(stringToCount: string): Number {
+        if (stringToCount === null) {
+            return 0;
+        }
+
+        let isOnlyWhitespace = /^\s+$/.test(stringToCount);
+        if (isOnlyWhitespace) {
+            return 0;
+        }
+
+        let normalizedString = this.sanitizeInputString(stringToCount);
+        let normalizedStringArray = normalizedString.split(' ');
+
+        normalizedStringArray = normalizedStringArray.filter(word => {
+            return word.trim().length;
+        });
+
+        let uniqueWordArray = this.returnDistinctWords(normalizedStringArray);
+        return uniqueWordArray.length;
+    }
+
+    private returnDistinctWords(wordArray) {
+        let uniqueWordArray = [];
+        for (let word of wordArray) {
+            if (!uniqueWordArray.includes(word)) {
+                uniqueWordArray.push(word);
+            }
+        }
+        return uniqueWordArray;
+    }
+
+    private sanitizeInputString(str: string): string {
+        let sanitizedString = str.toLowerCase();
+        sanitizedString = this.removeAccents(sanitizedString);
+        return this.removePunctuation(sanitizedString);
+    }
+
+    private removeAccents(str): string {
+        return str.normalize('NFD').replace(/[\u0080-\u036f]/g, '');
+    }
+
+    private removePunctuation(str): string {
+        return str.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g, '');
+    }
 
 };
